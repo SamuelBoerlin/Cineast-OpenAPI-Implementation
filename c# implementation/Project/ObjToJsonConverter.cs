4a2d620efe484a89da7f068a222953563a90ffc6
SamuelBoerlin <samuel.boerlin@stud.unibas.ch>
2020-01-07 19:53:47 +0100

    Added C# cineast openapi spec implementation

---
+++ c# implementation/Project/ObjToJsonConverter.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using ObjLoader.Loader.Loaders;
+using System.IO;
+
+namespace Cineast_OpenAPI_Implementation
+{
+    class ObjToJsonConverter
+    {
+        public static string Convert(FileStream stream)
+        {
+            var factory = new ObjLoaderFactory();
+            var loader = factory.Create();
+
+            var result = loader.Load(stream);
+
+            var json = new JObject();
+            var jsonVertices = new JArray();
+            dynamic dynJson = json;
+            dynJson.vertices = jsonVertices;
+
+            foreach(var group in result.Groups)
+            {
+                foreach(var face in group.Faces)
+                {
+                    for (int i = 0; i < face.Count; i++)
+                    {
+                        var vert = result.Vertices[face[i].VertexIndex - 1];
+                        jsonVertices.Add(vert.X);
+                        jsonVertices.Add(vert.Y);
+                        jsonVertices.Add(vert.Z);
+                    }
+                }
+            }
+
+            return json.ToString();
+        }
+    }
+}
---
+++ c# implementation/Project/Program.cs
@@ -0,0 +1,73 @@
+﻿using IO.Swagger.Api;
+using IO.Swagger.Client;
+using IO.Swagger.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cineast_OpenAPI_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start API");
+
+            Configuration.Default.BasePath = "http://192.168.56.101:4567/";
+
+            Apiv1Api api = new Apiv1Api();
+
+            var categories = new List<string>
+            {
+                "sphericalharmonicshigh"
+            };
+
+            var testModelJson = "";
+            using (FileStream fs = File.OpenRead("../../cube.obj"))
+            {
+                testModelJson = ObjToJsonConverter.Convert(fs);
+            }
+
+            var testModelData = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(testModelJson));
+
+            var terms = new List<QueryTerm>
+            {
+                new QueryTerm(categories, QueryTerm.TypeEnum.MODEL3D, "data:application/3d-json," + testModelData)
+            };
+
+            var components = new List<QueryComponent>
+            {
+                new QueryComponent(terms)
+            };
+
+            var conf = new QueryConfig();
+
+            Console.WriteLine("Start Similarity Query Request");
+
+            //Execute a similarity query
+            var response = api.ApiV1FindSegmentsSimilarPost(new SimilarityQuery(components, conf));
+
+            Console.WriteLine("Finished Similarity Query Request");
+
+            Console.WriteLine("Results:");
+            Console.WriteLine();
+
+            foreach (var similarityResult in response.Results)
+            {
+                foreach (var similarityContent in similarityResult.Content)
+                {
+                    Console.WriteLine("Object ID: " + similarityContent.Key + ", Similarity Score: " + similarityContent.Value);
+
+                    //TODO Not sure what the "_000001" appended after the returned object ID is, but this API call only works with it removed..
+                    var mediaObjectResult = api.ApiV1FindObjectByAttributeValueGet("id", similarityContent.Key.Substring(0, similarityContent.Key.Length - 7));
+                    foreach (var mediaObjectContent in mediaObjectResult.Content)
+                    {
+                        Console.WriteLine("Object Data: " + mediaObjectContent);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
