f3fe81a3c32aa990a681e22ae7bd57cea8da3d93
SamuelBoerlin <samuel.boerlin@stud.unibas.ch>
2020-01-08 16:22:56 +0100

    - Program can now download thumbnails and objects from seperate HTTP server or Cineast with CineastObjectDownloader - Added thumbnail and object GET requests to API spec - Fixed API spec for 'mediatype'

--- c# implementation/Project/Program.cs
+++ c# implementation/Project/Program.cs
@@ -4,18 +4,38 @@ using IO.Swagger.Model;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Cineast_OpenAPI_Implementation
 {
     class Program
     {
+
+
         static void Main(string[] args)
+        {
+            Task.Run(async () => await Run()).GetAwaiter().GetResult();
+        }
+
+        private static async Task Run()
         {
             Console.WriteLine("Start API");
 
-            Configuration.Default.BasePath = "http://192.168.56.101:4567/";
+            var baseApiUrl = "http://192.168.56.101:4567/";
+            var baseFileUrl = "http://192.168.56.101:8000/";
 
-            Apiv1Api api = new Apiv1Api();
+            Configuration.Default.BasePath = baseApiUrl;
+
+            var downloader = new CineastObjectDownloader
+            {
+                HostBaseUrl = baseFileUrl,
+                HostObjectsPath = "data/3d/:p",
+                HostThumbnailsPath = "thumbnails/:o/:s:x",
+                UseCineastServer = false //Cineast's thumbnail resolver is currently broken
+            };
+
+            var api = new Apiv1Api();
 
             var categories = new List<string>
             {
@@ -56,13 +76,37 @@ namespace Cineast_OpenAPI_Implementation
             {
                 foreach (var similarityContent in similarityResult.Content)
                 {
-                    Console.WriteLine("Object ID: " + similarityContent.Key + ", Similarity Score: " + similarityContent.Value);
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine("Segment ID: " + similarityContent.Key + ", Similarity Score: " + similarityContent.Value);
 
-                    //TODO Not sure what the "_000001" appended after the returned object ID is, but this API call only works with it removed..
-                    var mediaObjectResult = api.ApiV1FindObjectByAttributeValueGet("id", similarityContent.Key.Substring(0, similarityContent.Key.Length - 7));
-                    foreach (var mediaObjectContent in mediaObjectResult.Content)
+                    var segmentsResult = api.ApiV1FindSegmentsByIdPost(new IdList(new List<string>() { similarityContent.Key }));
+
+                    foreach (var segmentContent in segmentsResult.Content)
                     {
-                        Console.WriteLine("Object Data: " + mediaObjectContent);
+                        Console.WriteLine("Object ID: " + segmentContent.ObjectId);
+
+                        var mediaObjectResult = api.ApiV1FindObjectByAttributeValueGet("id", segmentContent.ObjectId);
+                        foreach (var mediaObjectContent in mediaObjectResult.Content)
+                        {
+                            Console.WriteLine("Object Data: " + mediaObjectContent);
+
+                            using (var stream = await downloader.RequestObjectAsync(api, mediaObjectContent, segmentContent))
+                            using (var reader = new StreamReader(stream))
+                            {
+
+                                string text = reader.ReadToEnd();
+
+                                Console.WriteLine("Downloaded Object: ");
+
+                                var lines = text.Split('\n');
+                                int maxLines = Math.Min(lines.Length, 8);
+                                for (int i = 0; i < maxLines; i++)
+                                {
+                                    Console.WriteLine(lines[i]);
+                                }
+                                Console.WriteLine("...");
+                            }
+                        }
                     }
                 }
             }
